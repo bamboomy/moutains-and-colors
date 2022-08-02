25fcff45158fa4095ee18f6adfd0ebd2f34f3aca
gonebergen <sander.gone.bergen@gmail.com>
2022-08-02 10:14:06 +0200

    playable

--- pagez/lobby.php
+++ pagez/lobby.php
@@ -24,7 +24,6 @@ $result = $conn->query($sql);
 
 <html>
 	<head>
-		<link rel="manifest" href="manifest.json">
 		<script src="../frameworkz/js/jquery-3.6.0.min.js"></script>
 		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
 		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
@@ -99,6 +98,8 @@ $result = $conn->query($sql);
 
 		</style>
 		<script>
+		
+			screen.orientation.lock('landscape');
 
 			function updateOnline() {
 
